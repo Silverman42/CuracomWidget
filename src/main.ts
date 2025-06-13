695a9395774e0ec24b09657faf15fca81d13ba0a
Sylvester Nkeze <snkeze9@gmail.com>
2025-06-13 09:44:14 +0100

    Update: Update widget setup

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,32 @@ import './assets/main.css'
 
 import { createApp, defineCustomElement } from 'vue'
 import App from './App.ce.vue'
+import { loadEnv } from 'vite'
 
 const CuracomPlugin = defineCustomElement(App)
 customElements.define('curacom-plugin', CuracomPlugin)
 // createApp(App).mount('#app')
+
+const env = loadEnv('production', process.cwd())
+
+function loadCss() {
+  const style = document.createElement('link')
+  style.rel = 'stylesheet'
+  style.href = env.BASE_URL + '/assets/widget-index.css'
+  document.head.appendChild(style)
+}
+
+function addPlugin() {
+  const body = document.querySelector('body')
+  const plugin = document.createElement('curacom-plugin')
+  body?.appendChild(plugin)
+}
+
+function prepareWidget() {
+  if (env.ENV === 'production') {
+    loadCss()
+    addPlugin()
+  }
+}
+
+prepareWidget()
